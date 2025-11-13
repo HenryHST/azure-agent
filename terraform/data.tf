9dfccf61971c6bcd4a4b60218fe76d38cc361c30
Henry Stadthagen <55046769+HenryHST@users.noreply.github.com>
2025-11-13 21:39:50 +0100

    bugfix: change tf files

--- terraform/data.tf
+++ terraform/data.tf
@@ -1,12 +1,12 @@
 ###
-data "azuredevops_agent_pool" "example" {
+data "azuredevops_agent_pool" "agent_pool_1" {
   name = "HomeLab Pool"
 }
-data "azuredevops_group" "example" {
+data "azuredevops_group" "reviewer_group" {
   project_id = data.azuredevops_project.example.id
   name       = "Reviewer"
 }
 
-data "azuredevops_project" "example" {
+data "azuredevops_project" "template" {
   name = "Template Project"
 }

89a957a6613dbd645118ac5251ee8877ec8c5de9
Henry Stadthagen <55046769+HenryHST@users.noreply.github.com>
2025-12-08 22:31:47 +0100

    cicd: add new variables for azure

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -8,3 +8,27 @@ variable "token" {
   sensitive   = true
   type        = string
 }
+variable "azure_subscription_id" {
+  description = "Azure Subscription ID"
+  type        = string
+}
+
+variable "environment" {
+  description = "Environment"
+  type        = string
+}
+
+variable "location" {
+  description = "Azure Region"
+  type        = string
+}
+
+variable "location_short" {
+  description = "Azure Region Short"
+  type        = string
+}
+
+variable "common_name" {
+  description = "Common Name"
+  type        = string
+}
